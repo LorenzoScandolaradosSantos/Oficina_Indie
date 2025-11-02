987ef80401e80373392dfb3b48c3ab346b55a44d
LorenzoScandolaradosSantos <lorenzo.ss2007@aluno.ifsc.edu.br>
2025-11-02 14:45:01 -0300

    Basic sum
    A simple code that gets that sums the int numbers of the X and Y variables, printing them on the console upon execution.

---
+++ Exercises/Assets/Scenes/Basic_sum/Basic_sum_script.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class Find_biggest_number_script : MonoBehaviour
+{
+    public int X = 5;
+    public int Y = 7;
+
+    void Start()
+    {
+        Debug.Log(X +" + " + Y +" = " + (X+Y));
+    }
+}
