1359ecd95bc943e015d93938e6d4712fd185671e
LorenzoScandolaradosSantos <lorenzo.ss2007@aluno.ifsc.edu.br>
2025-11-09 09:46:50 -0300

    Odds or evens 2
    An odds or evens game where you play against the computer, with the integration of UI.

--- Exercises/Assets/Scenes/Odds_or_evens_2/Odds_or_evens_2_script.cs
+++ Exercises/Assets/Scenes/Odds_or_evens_2/Odds_or_evens_2_script.cs
@@ -1,16 +1,60 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Odds_or_evens_2_script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public TMP_Text player_1_display_name;
+
+    public TMP_InputField player_1_name;
+    public TMP_InputField player_1_number;
+    public TMP_InputField player_1_choice;
+
+
+    public int player_2_number;
+    public TMP_Text player_2_display_number;
+
+    
+    public TMP_Text result;
+
+
+
+    public void Play()
     {
-        
+        Player_2_action();
+
+        player_1_display_name.text = player_1_name.text;
+
+
+        if (player_1_choice.text == "odds" && ((int.Parse(player_1_number.text)) + player_2_number) % 2 == 1)
+        {
+            result.text = $"{player_1_name.text} won choosing {player_1_choice.text} and playing {player_1_number.text}!";
+        }
+
+        else if (player_1_choice.text == "odds" && ((int.Parse(player_1_number.text)) + player_2_number) % 2 == 0)
+        {
+            result.text = $"Player 2 won playing {player_2_number}!";
+        }
+
+        else if (player_1_choice.text == "evens" && ((int.Parse(player_1_number.text)) + player_2_number) % 2 == 0)
+        {
+            result.text = $"{player_1_name.text} won choosing {player_1_choice.text} and playing {player_1_number.text}!";
+        }
+
+        else if (player_1_choice.text == "evens" && ((int.Parse(player_1_number.text)) + player_2_number) % 2 == 1)
+        {
+            result.text = $"Player 2 won playing {player_2_number}!";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void Player_2_action()
     {
-        
+        player_2_number = Random.Range(1, 6);
+        player_2_display_number.text = player_2_number.ToString();
     }
 }
